5e847d962f7c638673abc0376afd23a9240d433a
Jacqueline Gwynn <jacquelinegwynn@Jacquelines-MacBook-Air.local>
2019-05-04 13:49:13 -0400

    update file to fix semi-colon missing and render

--- cartoon-click/src/App.js
+++ cartoon-click/src/App.js
@@ -2,7 +2,6 @@ import React , {Component } from 'react';
 import Card from "./components/Card"; 
 import Wrapper from "./components/Wrapper"; 
 import cartoons from "./cartoon.json"; 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -17,7 +16,7 @@ class App extends Component {
   };
   
   clickCartoon = id => { 
-    const shuffle = this.shuffle(cartoons); 
+    let shuffle = this.shuffle(cartoons); 
     this.setState({cartoons: shuffle}); 
     if(this.state.clickedArray.includes(id)) {
       this.setState({ score:0, clickedArray: [], message: "I am Sorry that is Incorrect !! Please click an image to try again",scramble:"true"});
@@ -40,24 +39,30 @@ class App extends Component {
       }
       return picturesArray; 
     }; 
-
+  }
     render() {
-      // return (
-      //   <div className="App"></div>
-          {/* <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+      return (
+        <div>
+          <header className="App-header">
             <h1 className="App-title">Cartoon Clicky Game!!</h1>
-          </header> */}
-          {/* <h3 className="App-Intro">
-            <strong>Click on an image to receive point, but do not click it twice or you will lose</strong>
-            <p className="score"><strong>Score: {this.state.score} | TopScore: {this.state.topScore}</strong></p>
-            <p className="message">{this.state.message}</p>
-          </h3> */}
-          {/* <Wrapper
-          shakeWrapper = {this.state.shakeit}
-          cartoons= 
-            {this.state.cartoons.map(cartoon => (
-              <Card 
+            <h4 className="App-Intro">
+              <strong>
+                Click on an image to receive point, but do not click it
+                twice or you will lose
+              </strong>
+              <p className="score">
+                <strong>
+                  Score: {this.state.score} | TopScore:{" "}
+                  {this.state.topScore}
+                </strong>
+              </p>
+              <p className="message">{this.state.message}</p>
+            </h4>
+          </header>
+          <Wrapper
+            shakeWrapper={this.state.shakeit}
+            cartoons={this.state.cartoons.map(cartoon => (
+              <Card
                 clickCartoon={this.clickCartoon}
                 id={cartoon.id}
                 key={cartoon.id}
@@ -65,16 +70,17 @@ class App extends Component {
                 image={cartoon.image}
               />
             ))}
-          /> */}
-          {/* <footer className="footer">
-        <div className="container">
-          <span className="text-muted">J Gwynn Cartoon Clicky Game - React app - &copy; 2019</span>
+          />
+          <footer className="footer">
+            <div className="container">
+              <span className="text-muted">
+                J Gwynn Cartoon Clicky Game - React app - &copy; 2019
+              </span>
+            </div>
+          </footer>
         </div>
-      </footer>
-        </div> */}
-      {/* );
+      );
+      }
     }
-  }
-} */}
 
 export default App;
--- cartoon-click/src/components/Wrapper/Wrapper.js
+++ cartoon-click/src/components/Wrapper/Wrapper.js
@@ -1,24 +1,7 @@
 import React from "react";
 import "./Wrapper.css";
 
-const Wrapper = props => <div className = {props.shakeWrapper==="true" ? "wrapperShake" : "wrapper"}>{props.pictures} </div>;
+const Wrapper = props => 
+<div className={props.shakeWrapper==="true" ? "wrapperShake": "wrapper"}>{props.pictures} </div>;
 
 export default Wrapper;
-
-
-
-
-
-// import React from "react"; 
-// import "./Wrapper.css"; 
-
-// function Wrapper (props) {
-//     return (
-//         <header className = "header">
-//             <h1>Cartoon Click Game! </h1>
-//             <p>Click on an image to earn point, but don't click it twice</p>
-//         </header>
-//     )
-// }; 
-
-// export default Wrapper; 
